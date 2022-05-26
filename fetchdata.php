692a412e7c5f854e57b2ae4835c81c694a28eddc
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-05-26 20:13:42 +0530

    Update fetchdata.php

--- fetchdata.php
+++ fetchdata.php
@@ -1,15 +1,15 @@
 <?php
 	require 'vendor/autoload.php';
 	// connect to mongodb
-	$client = new MongoDB\Client('mongodb+srv://SmithaMenon:Verity123@cluster0.pgpsl.mongodb.net/?retryWrites=true&w=majority');
+	//$client = new MongoDB\Client('mongodb+srv://SmithaMenon:Verity123@cluster0.pgpsl.mongodb.net/?retryWrites=true&w=majority');
 	
 	echo "Connection to database successfully";
 	// select a database
-	$db = $client->airport_db;
+	//$db = $client->airport_db;
 	
 	echo ",   Database airport_db selected";
    
-	$collect = $db->test;
+	//$collect = $db->test;
 	echo ",     collection test selected";
 	
 	//$cursor = $collect->find();
