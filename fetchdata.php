7f82ff8474c708e9295fd8d1b08cc150af24a70e
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-05-26 20:16:02 +0530

    Update fetchdata.php

--- fetchdata.php
+++ fetchdata.php
@@ -1,7 +1,7 @@
 <?php
 	//require 'vendor/autoload.php';
 	// connect to mongodb
-	$client = new MongoDB\Client('mongodb+srv://SmithaMenon:Verity123@cluster0.pgpsl.mongodb.net/?retryWrites=true&w=majority');
+	//$client = new MongoDB\Client('mongodb+srv://SmithaMenon:Verity123@cluster0.pgpsl.mongodb.net/?retryWrites=true&w=majority');
 	
 	echo "Connection to database successfully";
 	// select a database
